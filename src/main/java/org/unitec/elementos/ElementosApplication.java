ce55add39fb385fc51b7410a62b5122975113e47
T-107 <T-107@PC193>
2018-01-30 21:55:05 -0600

    efra3

--- src/main/java/org/unitec/elementos/ElementosApplication.java
+++ src/main/java/org/unitec/elementos/ElementosApplication.java
@@ -1,5 +1,7 @@
 package org.unitec.elementos;
 
+import java.util.ArrayList;
+import static jdk.nashorn.internal.runtime.Debug.id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +18,23 @@ public class ElementosApplication implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         Mensajitos m=new Mensajitos("primero", "Mi primer mensajito con hibernate");
-                repoMensa.save(m);
+                //repoMensa.save(m);
+                
+                //Probamos el de buscar Todos
+                ArrayList<Mensajitos>mensajitos=
+                        (ArrayList<Mensajitos>)repoMensa.findAll();
+                for(Mensajitos mensa:mensajitos){
+                    System.out.print(mensa.getTitulo());
+                }
+                
+                //Ahora probaremos el de buscar por Id
+                Mensajitos m1=repoMensa.findOne( 1);
+                System.out.println(m1.getTitulo());
+                
+                //Actualizacion
+                repoMensa.save(new Mensajitos(1, "otro" ,"malo"));
+                
+                //Borrar
+                repoMensa.delete(1);
     }
 }
--- src/main/java/org/unitec/elementos/MiUI.java
+++ src/main/java/org/unitec/elementos/MiUI.java
@@ -9,11 +9,13 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
+import java.util.Arrays;
 
 /**
  *
